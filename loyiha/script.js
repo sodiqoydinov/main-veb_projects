7f8ddc148e8c0d55370c3717a8d48beb80acf959
Sodiq <sodiq7898@gmail.com>
2021-10-18 09:45:07 +0500

    Online maktab
    This is my first big project
    Name:Online mkatab
    Link:https://onlinemaktabproject.netlify.app/
    The project is incomplete !

---
+++ loyiha/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function()){
+
+    var coment=[
+        {"name":"Sodiq Oydinov","body":"This is coment"}
+    ]
+    for(var i=0;i<coment.length;i++){
+        $('#container').append(html)
+    }
+});
